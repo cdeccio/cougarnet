e83714fb1fb7976ce8edca72403af5d64c1f0baf
Casey Deccio <casey@deccio.net>
2022-07-04 07:09:09 -0600

    Add copyright notices

--- cougarnet/virtualnet/hostinit.py
+++ cougarnet/virtualnet/hostinit.py
@@ -1,4 +1,18 @@
-#!/usr/bin/python3
+# This file is a part of Cougarnet, a tool for creating virtual networks on
+# Linux.
+#
+# Copyright 2021-2022 Casey Deccio
+#
+# Cougarnet is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cougarnet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
 
 import argparse
 import grp
