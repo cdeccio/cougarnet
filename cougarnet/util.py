045563ed8cc535edc2c83f59cfdeca57f3b90e5e
Casey Deccio <casey@deccio.net>
2022-07-04 07:03:42 -0600

    Fix lint errors

--- cougarnet/util.py
+++ cougarnet/util.py
@@ -1,15 +1,28 @@
+'''
+Various utility functions for Cougarnet.
+'''
+
 import binascii
 import socket
 import subprocess
 import time
 
 def mac_str_to_binary(mac_str):
+    '''Given a MAC address in presentation format as a string, return the
+    equivalent bytes object.'''
+
     return binascii.unhexlify(mac_str.replace(':', ''))
 
 def mac_binary_to_str(mac_bin):
+    '''Given a bytes object, return the equivalent MAC address in presentation
+    format as a string.'''
+
     return ':'.join(['%02x' % b for b in mac_bin])
 
 def ip_str_to_binary(ip_str):
+    '''Given an IPv4 or IPv6 address in presentation format as a string, return
+    the equivalent bytes object.'''
+
     if ':' in ip_str:
         af = socket.AF_INET6
     else:
@@ -17,6 +30,9 @@ def ip_str_to_binary(ip_str):
     return socket.inet_pton(af, ip_str)
 
 def ip_binary_to_str(ip_bin):
+    '''Given a bytes object, return the equivalent IPv4 or IPv6 address in
+    presentation format as a string.'''
+
     if len(ip_bin) > 4:
         af = socket.AF_INET6
     else:
@@ -24,24 +40,37 @@ def ip_binary_to_str(ip_bin):
     return socket.inet_ntop(af, ip_bin)
 
 def pid_is_running(pid):
+    '''Return True if the process associated with a given pid (int) is running;
+    False otherwise.'''
+
     cmd = ['ps', '-p', str(pid)]
-    p = subprocess.run(cmd, stdout=subprocess.DEVNULL,
-            stderr=subprocess.STDOUT)
+    p = subprocess.run(cmd, check=False,
+            stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     return p.returncode == 0
 
 def kill(pid, sig, elevate_if_needed=False):
+    '''Send a signal (e.g., TERM, KILL) to a process.  If a permissions error
+    is detected, and elevate_if_needed is True, then send the same signal again
+    as root.  Return True if the signal was sent successfully; False
+    otherwise.'''
+
     cmd = ['kill', f'-{sig}', str(pid)]
     p = subprocess.run(cmd, stdout=subprocess.DEVNULL,
-            stderr=subprocess.PIPE)
+            stderr=subprocess.PIPE, check=False)
     if p.returncode != 0:
         stderr = p.stderr.decode('utf-8').lower()
+        #XXX this does not work for non-English
         if 'not permitted' in stderr and elevate_if_needed:
             cmd.insert(0, 'sudo')
-            p = subprocess.run(cmd, stdout=subprocess.DEVNULL,
-                    stderr=subprocess.DEVNULL)
+            p = subprocess.run(cmd, check=False,
+                    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return p.returncode == 0
 
 def kill_until_terminated(pid, elevate_if_needed=False):
+    '''Send TERM to a process.  If the process continues to run, then send
+    KILL.  In both cases, elevate if a permissions error is detected, and
+    elevate_if_needed is True.'''
+
     sigs = ('TERM', 'KILL')
     for sig in sigs:
         kill(pid, sig, elevate_if_needed=elevate_if_needed)
