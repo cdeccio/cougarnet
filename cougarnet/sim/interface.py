989d2c8a2f7638ee3abd832056fc9bd767020f1f
Casey Deccio <casey@deccio.net>
2022-07-04 07:11:00 -0600

    Add copyright notice

--- cougarnet/sim/interface.py
+++ cougarnet/sim/interface.py
@@ -1,3 +1,19 @@
+# This file is a part of Cougarnet, a tool for creating virtual networks on
+# Linux.
+#
+# Copyright 2021-2022 Casey Deccio
+#
+# Cougarnet is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cougarnet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
 class InterfaceInfo:
     def __init__(self, mac_addr, ipv4_addrs, ipv4_prefix_len,
             ipv6_addrs, ipv6_addr_link_local, ipv6_prefix_len, mtu):
