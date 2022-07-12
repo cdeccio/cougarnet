c562112c8ff821865c6138a2d8b7a7e04c1f9487
Casey Deccio <casey@deccio.net>
2022-07-12 16:23:45 -0600

    Update copyright notices

--- cougarnet/virtualnet/interface.py
+++ cougarnet/virtualnet/interface.py
@@ -1,17 +1,19 @@
-# This file is a part of Cougarnet, a tool for creating virtual networks on
-# Linux.
+# This file is a part of Cougarnet, a tool for creating virtual networks.
 #
-# Copyright 2021-2022 Casey Deccio
+# Copyright 2021-2022 Casey Deccio (casey@deccio.net)
 #
-# Cougarnet is free software; you can redistribute it and/or modify
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# Cougarnet is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
 #
 
 '''Classes for maintaining configurations related to network interfaces for
