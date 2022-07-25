c88f1e04400c3b6e44b4d657ae98779b129167b1
Casey Deccio <casey@deccio.net>
2022-07-25 14:14:21 -0600

    Add net/system helper

--- cougarnet/virtualnet/sys_helper.py
+++ cougarnet/virtualnet/sys_helper.py
@@ -16,10 +16,21 @@
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 #
 
+import argparse
+import asyncio
+import atexit
 import os
+import socket
 import subprocess
+import sys
 
 RUN_NETNS_DIR='/run/netns/'
+
+def _delete_softly(path):
+    try:
+        os.unlink(path)
+    except FileNotFoundError:
+        pass
 
 class NetConfigHelper:
     def __init__(self):
@@ -30,7 +41,7 @@ class NetConfigHelper:
     def add_link_veth(self, intf1, intf2):
         cmd = ['sudo', 'ip', 'link', 'add',
                 intf1, 'type', 'veth']
-        if intf2 is not None:
+        if intf2:
             cmd += ['peer', intf2]
 
         try:
@@ -229,8 +240,83 @@ class NetConfigHelper:
 
         return True
 
+    def handle_request(self, sock):
+        while True:
+            try:
+                msg, peer = sock.recvfrom(4096)
+            except BlockingIOError:
+                return
+            msg = msg.decode('utf-8')
+            parts = msg.split('|')
+            if parts[0] == 'add_link_veth':
+                status = self.add_link_veth(*parts[1:])
+            elif parts[0] == 'add_link_vlan':
+                status = self.add_link_vlan(*parts[1:])
+            elif parts[0] == 'add_link_bridge':
+                status = self.add_link_bridge(*parts[1:])
+            elif parts[0] == 'set_link_master':
+                status = self.set_link_master(*parts[1:])
+            elif parts[0] == 'set_link_up':
+                status = self.set_link_up(*parts[1:])
+            elif parts[0] == 'del_link':
+                status = self.del_link(*parts[1:])
+            elif parts[0] == 'touch_netns':
+                status = self.touch_netns(*parts[1:])
+            elif parts[0] == 'umount_netns':
+                status = self.umount_netns(*parts[1:])
+            elif parts[0] == 'del_netns':
+                status = self.del_netns(*parts[1:])
+            elif parts[0] == 'ovs_add_bridge':
+                status = self.ovs_add_bridge(*parts[1:])
+            elif parts[0] == 'ovs_del_bridge':
+                status = self.ovs_del_bridge(*parts[1:])
+            elif parts[0] == 'ovs_add_port':
+                status = self.ovs_add_port(*parts[1:])
+            elif parts[0] == 'disable_ipv6':
+                status = self.disable_ipv6(*parts[1:])
+            else:
+                status = False
+
+            if status:
+                msg = '1'.encode('utf-8')
+            else:
+                msg = '0'.encode('utf-8')
+            sock.sendto(msg, peer)
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--user', '-u',
+            action='store', type=str,
+            help='User that should own the socket')
+    parser.add_argument('socket',
+            action='store', type=str,
+            help='Socket path ')
+
+    args = parser.parse_args(sys.argv[1:])
+
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    try:
+        sock.bind(args.socket)
+    except OSError as e:
+        sys.stderr.write(f'Invalid path: {args.socket} ({str(e)})\n')
+        sys.exit(1)
+    atexit.register(_delete_softly, args.socket)
+    subprocess.run(['chmod', '700', args.socket], check=True)
+    if args.user is not None:
+        subprocess.run(['chown', args.user, args.socket], check=True)
+
+    sock.setblocking(False)
+
+    helper = NetConfigHelper()
+    loop = asyncio.get_event_loop()
+    loop.add_reader(sock, helper.handle_request, sock)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
 
 if __name__ == '__main__':
     main()
